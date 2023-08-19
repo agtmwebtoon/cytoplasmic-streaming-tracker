3a3b90f3441bce64e6d47f6f8de5d4455cabd955
Minhyung Lee <ss49132002@gmail.com>
2023-08-20 01:19:10 +0900

    fix filtering in main

--- main.py
+++ main.py
@@ -1,68 +1,30 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import scienceplots
-from matplotlib.animation import FuncAnimation
+import cv2
 
+# Path to the TIF image file
+tif_file_path = 'dataset/IT229148/IT229148_25_50fps_binning2_sample3(1).tif'
 
-# CSV 데이터 읽기
-data = pd.read_csv('dataset/2 pedestian_preproc.csv')
-num_rows = len(data)
+# Open the TIF image as a 'video'
+cap = cv2.VideoCapture(tif_file_path)
 
-init_damper = [0.022, 0.0051]
-init_ped = [-0.009, -0.0249]
+# Check if the TIF image was opened successfully
+if not cap.isOpened():
+    print("Error opening TIF file")
+    exit()
 
+# Loop through each frame in the TIF image
+while True:
+    ret, frame = cap.read()
+    # If the frame was not read successfully, break the loop
+    if not ret:
+        break
 
-# 스트리밍 데이터를 생성하는 함수
-def generate_data():
-    for i in range(num_rows):
-        row = data.iloc[i]
-        yield row['Time'], row['Value'], row['Time1'], row['Value1']
+    # Display or process the frame
+    cv2.imshow('TIF Frame', frame)
 
-# 스트리밍 데이터를 그래프로 표시하는 함수
-def update_graph(i):
-    if i >= num_rows:
-        # 데이터를 모두 표시한 경우 애니메이션 종료
-        ani.event_source.stop()
-        return
+    # Press 'q' to exit the loop
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
 
-    time, value, time1, value1 = next(data_stream)
-
-    x_data.append(i)
-    y_data.append(value - init_ped[1])
-
-    x1_data.append(i)
-    y1_data.append(value1 - init_ped[0])
-
-    if len(x_data) == 30:
-        x_data.pop(0)
-        y_data.pop(0)
-        x1_data.pop(0)
-        y1_data.pop(0)
-
-    ax.clear()
-    ax.plot(x_data, y_data, label='Inlet')
-    ax.plot(x1_data, y1_data, label='Outlet')
-    ax.set_title("Streaming Pressure Data")
-    ax.set_xlabel("TimeStamp")
-    ax.set_ylabel("Pressure(psi)")
-    ax.set_ylim(-0.05, 0.1)
-# Matplotlib Figure 및 Axes 생성
-fig, ax = plt.subplots()
-
-# 초기 데이터 리스트
-x_data = []
-y_data = []
-
-# 초기 데이터 리스트
-x1_data = []
-y1_data = []
-
-# 데이터 스트림 생성
-data_stream = generate_data()
-
-# 애니메이션 생성
-ani = FuncAnimation(fig, update_graph, frames=num_rows, interval=1, repeat=False)
-
-# 그래프 표시
-plt.show()
-
+# Release the TIF file capture object and close any open windows
+cap.release()
+cv2.destroyAllWindows()
--- opticalFlow/vector_farneback.py
+++ opticalFlow/vector_farneback.py
@@ -1,28 +1,41 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.neighbors import KernelDensity
+import scienceplots
+plt.style.use(['science','ieee'])
 
 first_flag = True
 roi_flag = True
 length = 0
+prev = None
+
+cap = cv2.VideoCapture("../dataset/IT283501l_25_50fps_binning2_sample4(2).avi")
+fps = cap.get(cv2.CAP_PROP_FPS)
+delay = int(1000 / fps)
+roi = [0, 0, 0, 0]
+center_of_roi = [0, 0]
+transposed_pos = [0, 0]
+mask = np.nan
 
 
-def drawFlow(img, flow, step=2, filtering=False):
+def drawFlow(img, flow, step=1, filtering=False):
     global min_dist
     h, w = flow.shape[:2]
 
-    idx_y, idx_x = np.mgrid[step/2:h:step, step/2:w:step].astype(int)
+    idx_y, idx_x = np.mgrid[step / 2:h:step, step / 2:w:step].astype(int)
     indicies = np.stack((idx_x, idx_y), axis=-1).reshape(-1, 2)
 
     if filtering:
-        min_dist = find_optical_distance(flow, 90, h)
-
+        min_dist = find_optical_distance(flow, 99.5, h)
+        print(min_dist)
+        pass
     for x, y in indicies:
-        dx, dy = flow[y, x].astype(int)
+        dy, dx = flow[y, x].astype(int)
         if filtering and np.sqrt(dx ** 2 + dy ** 2) > min_dist:
-            print(min_dist)
-            #x += center_of_roi[0] // 2
-            #y += center_of_roi[1] // 2
+            # print(min_dist)
+            # x += transposed_pos[0]
+            # y += transposed_pos[1]
             cv2.circle(img, (x, y), 1, (0, 255, 0), -1)
             cv2.arrowedLine(img, (x, y), (x + dx, y + dy), (0, 255, 0), 1, cv2.LINE_AA)
 
@@ -30,53 +43,78 @@ def drawFlow(img, flow, step=2, filtering=False):
             cv2.circle(img, (x, y), 1, (0, 255, 0), -1)
             cv2.arrowedLine(img, (x, y), (x + dx, y + dy), (0, 255, 0), 1, cv2.LINE_AA)
 
+
+def masking_circle(unmask_frame):
+    result_image = cv2.bitwise_and(unmask_frame, unmask_frame, mask=mask)
+
+    return result_image
+
+
 def find_optical_distance(pixel_dist, percent, pixel_size):
     global first_flag
     dists = []
     for i in range(pixel_size):
         for j in range(pixel_size):
-              dists.append(np.sqrt(pixel_dist[i][j][0] ** 2 + pixel_dist[i][j][1] ** 2))
+            dists.append(np.sqrt(pixel_dist[i][j][0] ** 2 + pixel_dist[i][j][1] ** 2))
 
     if first_flag:
         plt.plot(dists)
+        dists = np.array(dists).reshape(-1, 1)
         plt.show()
+
         first_flag = False
+        kde = KernelDensity(bandwidth=1, kernel='gaussian')
+        kde.fit(dists)
+
+        # x 범위 생성
+        x = np.linspace(min(dists), max(dists), 1000).reshape(-1, 1)
+
+        # KDE 계산
+        log_density = kde.score_samples(x)
+
+        # 시각화
+        plt.plot(x, np.exp(log_density), label='KDE')
+        plt.scatter(dists, np.zeros_like(dists), color='red', label='Data Points')
+        plt.title('Kernel Density Estimation')
+        plt.xlabel('x')
+        plt.ylabel('Probability Density')
+        plt.legend()
+        plt.grid(True)
+        plt.show()
 
     return np.percentile(dists, percent)
 
 
-
-prev = None
-
-cap = cv2.VideoCapture('../dataset/IT261516_25_50fps_binning2_sample2(1).avi')
-fps = cap.get(cv2.CAP_PROP_FPS)
-delay = int(1000/fps)
-roi = [0, 0, 0, 0]
-center_of_roi = [0, 0]
-
 while cap.isOpened():
+
     ret, frame = cap.read()
     if not ret: break
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     if roi_flag:
 
-        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 300, param1=150, param2=50, minRadius=120, maxRadius=0)
+        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 300, param1=150, param2=50, minRadius=0, maxRadius=0)
 
         if circles is not None and circles.all():
             for i in circles[0]:
+                # roi => (x_1, y_1), (x_2, y_2)
                 roi = [int(i[0] - i[2]), int(i[1] - i[2]), int(i[0] + i[2]), int(i[1] + i[2])]
                 center_of_roi = [(roi[0] + roi[2]) // 2, (roi[1] + roi[3]) // 2]
-                cv2.circle(frame, (int(i[0]), int(i[1])), int(i[2]), (255, 255, 255), 5)
 
+                # transposed_pos => (x_1, y_1)
+                transposed_pos = [roi[0], roi[1]]
+                mask = np.zeros(gray.shape[:2], dtype=np.uint8)
+                mask = cv2.circle(mask, (int(i[0]), int(i[1])), int(i[2]) - 20, (255, 255, 255), -1)
+
+                masked_image = masking_circle(gray)
             roi_flag = False
 
     else:
-        gray = gray[roi[0]:roi[2], roi[1]:roi[3]]
+        # gray = gray[roi[0]:roi[2], roi[1]:roi[3]]
         if prev is None:
-            prev = gray
+            prev = masking_circle(gray)
         else:
-
+            gray = masking_circle(gray)
             '''
             prev – first 8-bit single-channel input image.
             next – second input image of the same size and the same type as prev.
@@ -88,16 +126,16 @@ while cap.isOpened():
             poly_n – size of the pixel neighborhood used to find polynomial expansion in each pixel; larger values mean that the image will be approximated with smoother surfaces, yielding more robust algorithm and more blurred motion field, typically poly_n =5 or 7.
             poly_sigma – standard deviation of the Gaussian that is used to smooth derivatives used as a basis for the polynomial expansion; for poly_n=5, you can set poly_sigma=1.1, for poly_n=7, a good value would be poly_sigma=1.5.
             '''
-            flow = cv2.calcOpticalFlowFarneback(prev, gray, flow=0.5,
+            flow = cv2.calcOpticalFlowFarneback(prev, gray, flow=1,
                                                 pyr_scale=0.5,
-                                                levels=3,
-                                                winsize=10,
+                                                levels=5,
+                                                winsize=5,
                                                 iterations=7,
                                                 poly_n=13,
                                                 poly_sigma=1.1,
                                                 flags=cv2.OPTFLOW_FARNEBACK_GAUSSIAN)
 
-            drawFlow(frame, flow, filtering=True)
+            drawFlow(frame, flow, step=2, filtering=True)
             prev = gray
 
     cv2.imshow('OpticalFlow-Farneback', frame)
